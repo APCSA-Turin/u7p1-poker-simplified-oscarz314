d5483ccf987ce85c5781cb2c87cb065792bfeb74
Oscarz <111668068+oscarz314@users.noreply.github.com>
2025-02-27 17:03:15 +0000

    hello

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -1,5 +1,6 @@
 package com.example.project;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Player{
@@ -19,13 +20,18 @@ public class Player{
         
     }
 
-    public String playHand(ArrayList<Card> communityCards){      
+    public String playHand(ArrayList<Card> communityCards){
         return "Nothing";
     }
 
     public void sortAllCards(){} 
 
     public ArrayList<Integer> findRankingFrequency(){
+        Array<Integer> rankFrequency = new ArrayList<Integer>(Arrays.asList(new int[12]));
+
+        for(int i = 0; i < allCards.size(); i++){
+            allCards.get(i).getRank()
+        }
         return new ArrayList<>(); 
     }
 
