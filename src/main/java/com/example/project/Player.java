61a289f04702bc056cb800ee812d0a6926703b18
Oscarz <111668068+oscarz314@users.noreply.github.com>
2025-02-28 17:33:34 +0000

    eeee

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -17,26 +17,84 @@ public class Player{
     public ArrayList<Card> getAllCards(){return allCards;}
 
     public void addCard(Card c){
-        
+        hand.add(c);
     }
 
     public String playHand(ArrayList<Card> communityCards){
+        allCards = communityCards;
+
+        if(highCard() == true){
+            return "High Card";
+        }
+
         return "Nothing";
     }
 
-    public void sortAllCards(){} 
+    public void sortAllCards(){
+        for(int i = 0; i < allCards.size() - 1; i++){
+            int j = i + 1;
+            while(j > 0 && getRankInt(allCards.get(j)) < getRankInt(allCards.get(j - 1))){
+                Card temp = allCards.set(j, allCards.get(j - 1));
+                allCards.set(j - 1, temp);
+            }
+        }
+    } 
 
     public ArrayList<Integer> findRankingFrequency(){
-        Array<Integer> rankFrequency = new ArrayList<Integer>(Arrays.asList(new int[12]));
+        ArrayList<Integer> rankFrequency = new ArrayList<>();
+        for (int i = 0; i < rankFrequency.size(); i++){
+            rankFrequency.add(0);
+        }
 
         for(int i = 0; i < allCards.size(); i++){
-            allCards.get(i).getRank()
+            rankFrequency.set(getRankInt(allCards.get(i)), rankFrequency.get(getRankInt(allCards.get(i))) + 1);
         }
-        return new ArrayList<>(); 
+
+        return rankFrequency; 
     }
 
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> suitFrequency = new ArrayList<>();
+        for (int i = 0; i < suitFrequency.size(); i++){
+            suitFrequency.add(0);
+        }
+
+        for (int i = 0; i < allCards.size(); i++){
+            if (allCards.get(i).getSuit().equals(Utility.getSuits()[0])){
+                suitFrequency.set(0, suitFrequency.get(0) + 1);
+            }
+            else if (allCards.get(i).getSuit().equals(Utility.getSuits()[1])){
+                suitFrequency.set(1, suitFrequency.get(1) + 1);
+            }
+            else if (allCards.get(i).getSuit().equals(Utility.getSuits()[2])){
+                suitFrequency.set(2, suitFrequency.get(2) + 1);
+            }
+            else if (allCards.get(3).getSuit().equals(Utility.getSuits()[3])){
+                suitFrequency.set(3, suitFrequency.get(3) + 1);
+            }
+        }
+
+        return suitFrequency; 
+    }
+
+    public int getRankInt(Card card){
+        return Utility.getRankValue(card.getRank());
+    }
+
+    public Boolean highCard(){
+        for(int i = 0; i < allCards.size(); i++){
+            if(hand.get(0) != allCards.get(i) && hand.get(1) != allCards.get(i)){
+                if(getRankInt(hand.get(0)) > getRankInt(allCards.get(i)) || getRankInt(hand.get(1)) > getRankInt(allCards.get(i))){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    Public String countRepeats(){
+        int highestRepeat
     }
 
    
